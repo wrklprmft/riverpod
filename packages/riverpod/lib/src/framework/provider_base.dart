a6a00271faa85b1a25930e4f9baab5500219cbbc
Remi Rousselet <darky12s@gmail.com>
2022-06-13 14:53:42 +0200

    Continue dependencies check

--- packages/riverpod/lib/src/framework/provider_base.dart
+++ packages/riverpod/lib/src/framework/provider_base.dart
@@ -24,6 +24,7 @@ typedef FamilyCreate<T, R extends Ref, Arg> = T Function(
 );
 
 /// A function that reads the state of a provider.
+@Deprecated('Use Ref instead')
 typedef Reader = T Function<T>(ProviderBase<T> provider);
 
 /// A base class for _all_ providers.
